e87d3d3331601c46c00f0a33dcc3321387868d9b
= <=>
2025-09-14 23:46:53 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "datadog" {
+  api_key = var.DATADOG_API_KEY
+  app_key = var.DATADOG_APP_KEY
+}
+
+resource "datadog_monitor" "example_monitor" {
+  name    = "Example Monitor"
+  type    = "metric alert"
+  query   = "avg(last_5m):avg:system.cpu.user{host:host0} > 80"
+  message = "CPU usage is above 80% on host0."
+  tags    = ["env:production"]
+
+  thresholds {
+    critical = 80
+  }
+}
